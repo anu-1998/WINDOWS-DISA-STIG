119a799c9b1adcd070a4a30b06afeeeed9b47b5b
Anaswara Raj <75777897+anu-1998@users.noreply.github.com>
2026-04-21 19:55:03 +0100

    Update WN11-AC-000035.ps1

--- WN11-AC-000035.ps1
+++ WN11-AC-000035.ps1
@@ -1,3 +1,33 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that the Windows Installer feature 
+    "Always install with elevated privileges" is disabled.
+
+.NOTES
+    Author          : Anaswara Raj
+    LinkedIn        : https://www.linkedin.com/in/anaswara-raj/
+    GitHub          : https://github.com/anu-1998
+    Date Created    : 2026-02-01
+    Last Modified   : 2026-02-03
+    Version         : 1.1
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000035
+
+.TESTED ON
+    Date(s) Tested  : 2026-02-04
+    Tested By       : Anaswara Raj
+    Systems Tested  : Windows 11
+    PowerShell Ver. : 5.1 / 7.x
+
+.USAGE
+    Run this script as Administrator.
+    Example syntax:
+    PS C:\> .\remediation_WN11-CC-000035.ps1
+#>
+
+# STIG ID: WN11-CC-000035
+# Description: Set Minimum Password Length to 14 characters
 
 
 
